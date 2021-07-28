c0e17ddf1864077015b8ceb12c3b7f6ae35e5f3e
Dan Murphy <danielmurph8@gmail.com>
2021-07-27 21:42:52 -0400

    add additional constants

--- lib/algosdk/utils/constants.rb
+++ lib/algosdk/utils/constants.rb
@@ -1,4 +1,7 @@
 module Constants
   "" "int: how long addresses are in bytes" ""
   KEY_LEN_BYTES = 32
+  NO_AUTH = []
+  ALGOD_AUTH_HEADER = "X-Algo-API-Token"
+  UNVERSIONED_PATHS = ["/health", "/versions", "/metrics", "/genesis"]
 end
