6644db6e189b22eeaa59035c757681c3b3f1c3f4
Dan Murphy <danielmurph8@gmail.com>
2021-07-28 22:35:52 -0400

    add custom error classes

---
+++ lib/algosdk/error.rb
@@ -0,0 +1,17 @@
+module AlgoSDK
+  module Errors
+    class AlgodRequestError < StandardError
+      def initialize(requrl)
+        @requrl = requrl
+        @message = "Error while building and executing request: #{@requrl}"
+        super(@message)
+      end
+    end
+
+    class ArgsError < StandardError
+      def initialize(message)
+        super(message)
+      end
+    end
+  end
+end
